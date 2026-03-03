5d7afe58b8b17a00316d32884e38b1cf1a6b3e72
Trust Onyekwere <trustonyekwere151@gmail.com>
2026-03-03 21:23:46 +0100

    fixed blog responsiveness issue

--- app/components/sections/Blog.tsx
+++ app/components/sections/Blog.tsx
@@ -32,13 +32,13 @@ const Blog = () => {
         <div className="bg-white/90 py-10" id="blog">
             <div className="mx-auto max-w-6xl px-6 pb-7 py-10">
 
-                <div className="flex items-center">
+                <div className="md:flex items-center">
                     <div>
                         <p className="text-orange-500 text-lg">Our Blog</p>
-                        <h1 className="text-5xl text-[#050B1C] font-bold mb-3">Browse Our Blogs</h1>
+                        <h1 className="text-5xl text-[#050B1C] font-bold py-3">Browse Our Blogs</h1>
                         <p className="text-gray-500 max-w-[70%] text-lg">Welcome to our real estate blog, where we delve into a world of insights, tips, and inspiration for both buyers and sellers.</p>
                     </div>
-                    <div className="shrink-0">
+                    <div className="shrink-0 my-5 md:my-auto">
                         <ButtonLink text="View All Posts" href="/listing" />
                     </div>
                 </div>
@@ -55,7 +55,7 @@ const Blog = () => {
                                     <Image
                                         src="/blog.jpg" 
                                         alt="home interior"
-                                        width={400}    
+                                        width={600}    
                                         height={100}    
                                         className="rounded-xl mb-2"
                                         priority
@@ -89,13 +89,13 @@ const Blog = () => {
                 </div>
 
                 <div className="pt-10 justify-center items-center flex">
-                    <div className="bg-[#050B1C] rounded-4xl w-3xl h-50 px-16 justify-between flex items-center hover:scale-105 transition-all duration-400 cursor-pointer">
-                        <div className="flex items-center justify-between w-full">
+                    <div className="bg-[#050B1C] rounded-4xl w-full md:w-3xl h-55 md:h-50 px-8 justify-between flex items-center hover:scale-105 transition-all duration-400 cursor-pointer">
+                        <div className="flex items-center justify-between gap-3">
                             <div>
-                                <h1 className="text-4xl font-bold min-w-100">Get A Fit Lifestyle <br /> Easily And Fast Here.</h1>
+                                <h1 className="text-2xl md:text-4xl font-bold w-full">Get A Fit Lifestyle <br /> Easily And Fast Here.</h1>
                             </div>
                             <div className="bg-gray-700 rounded-full p-3 border-dotted border-4 border-orange-500">
-                                <ArrowUpRightIcon size={40} />
+                                <ArrowUpRightIcon size={35} />
                             </div>
                         </div>
                     </div>
