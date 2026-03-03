4376551977d3f1b44e720e71da3ef4ae25a161af
Trust Onyekwere <trustonyekwere151@gmail.com>
2026-03-03 21:24:16 +0100

    added proper spacing

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,4 +1,3 @@
-import {FacebookIcon, InstagramIcon, TwitterIcon} from "lucide-react"
 import Link from "next/link"
 import ButtonLink from "../ui/Button"
 
@@ -9,7 +8,7 @@ export default function Footer() {
         <>
             <section className="bg-[#050B1C]">
                 <div className="mx-auto max-w-6xl px-6 pt-18 pb-5">
-                    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr_1fr_1fr] gap-6">
+                    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr_1fr_1fr] gap-6 pb-5">
                         <div>
                             {/* Logo */}
                             <Link href="#hero" className="text-xl font-bold text-white">
@@ -17,37 +16,37 @@ export default function Footer() {
                             </Link>
                             <p className="mt-2 text-gray-300">With a commitment to excellence, we're here to guide you through every step of your real estate journey.</p>
                             <div className="mt-4">
-                              <ButtonLink text="Get Started" href="/get-started" />
+                                <ButtonLink text="Get Started" href="/get-started" />
                             </div>
                         </div>
                         <div>
                             <h1 className="font-semibold text-xl">Explore</h1>
-                            <ul className="text-gray-400 space-y-1 mt-2">
-                              <li><a href="#features" className="hover:text-gray-300">Sell Your Home</a></li>
-                              <li><a href="#features" className="hover:text-gray-300">Rent A Home</a></li>
-                              <li><a href="#features" className="hover:text-gray-300">Buy A Home</a></li>
+                            <ul className="text-gray-400 space-y-3 mt-2">
+                                <li><a href="#features" className="hover:text-gray-300">Sell Your Home</a></li>
+                                <li><a href="#features" className="hover:text-gray-300">Rent A Home</a></li>
+                                <li><a href="#features" className="hover:text-gray-300">Buy A Home</a></li>
                             </ul>
                         </div>
                         <div>
                             <h1 className="font-semibold text-xl">Services</h1>
-                            <ul className="text-gray-400 space-y-1 mt-2">
-                              <li><a href="#about" className="hover:text-gray-300">About Us</a></li>
-                              <li><a href="#features" className="hover:text-gray-300">Features</a></li>
-                              <li><a href="#property" className="hover:text-gray-300">Property</a></li>
-                              <li><a href="#blog" className="hover:text-gray-300">Blog</a></li>
+                            <ul className="text-gray-400 space-y-3 mt-2">
+                                <li><a href="#about" className="hover:text-gray-300">About Us</a></li>
+                                <li><a href="#features" className="hover:text-gray-300">Features</a></li>
+                                <li><a href="#property" className="hover:text-gray-300">Property</a></li>
+                                <li><a href="#blog" className="hover:text-gray-300">Blog</a></li>
                             </ul>
                         </div>
                         <div>
                             <h1 className="font-semibold text-xl">Contact Us</h1>
-                            <ul className="text-gray-400 space-y-1 mt-2">
-                              <li><a href="https://wa.me/+2348108212511" className="hover:text-gray-300">+234 810 821 2511</a></li>
-                              <li><a href="mailto:trustonyekwere151@gmail.com" className="hover:text-gray-300">trustonyekwere151@gmail.com</a></li>
+                            <ul className="text-gray-400 space-y-3 mt-2">
+                                <li><a href="https://wa.me/+2348108212511" className="hover:text-gray-300">+234 810 821 2511</a></li>
+                                <li><a href="mailto:trustonyekwere151@gmail.com" className="hover:text-gray-300">trustonyekwere151@gmail.com</a></li>
                             </ul>
                         </div>
                     </div>
                     <br />
                     <div className="border-t flex justify-center items-center pt-6 text-gray-500">
-                      <p>&copy; {currentYear} Real Estate. All Rights Reserved.</p>
+                        <p>&copy; {currentYear} Real Estate. All Rights Reserved.</p>
                     </div>
                 </div>
             </section>
